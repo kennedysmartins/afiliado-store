d20fc23ebf8eb4c9635c079f96f47589ded8c514
KillerDogs <kenybustamante2@hotmail.com>
2024-01-07 18:34:44 -0300

    function handleSearch and className

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,30 @@
+"use client"
 import React from "react"
 import { Input } from "./ui/input"
+import { useRouter } from "next/navigation"
+import { cn } from "@/lib/utils"
 
-const Search = () => {
+const Search = ({ className }: { className?: string }) => {
+  const router = useRouter()
+  const [searchTerm, setSearchTerm] = React.useState("")
+
+  const handleSearch = () => {
+    // Navega para a página de pesquisa com o termo na URL
+    router.push(`/search?term=${searchTerm}`)
+  }
   return (
     <div>
       <Input
         type="search"
         placeholder="Pesquisar..."
-        className="md:w-[100px] lg:w-[300px]"
+        className={cn("md:w-[100px] lg:w-[300px]", className)}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={(e) => {
+          // Se pressionar Enter, realiza a pesquisa
+          if (e.key === "Enter") {
+            handleSearch()
+          }
+        }}
       />
     </div>
   )
