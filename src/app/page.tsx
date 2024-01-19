a8992df31661fcc7d1b37f3e43132746c92f71e2
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-19 04:21:27 -0300

    initial style homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+import { CarouselHome } from "@/components/CarouselHome"
 import Navbar from "@/components/Navbar"
+import ProductCard from "@/components/ProductCard"
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="mx-auto max-w-5xl text-2xl gap-2 my-10">
-        <h1>Hello</h1>
+      <main className="mx-auto sm:w-[83rem] text-2xl gap-2 my-10 w-full">
+        <div className="flex justify-center items-center w-full">
+        <CarouselHome />
+        </div>
+
+        <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-8 w-full mx-auto sm:flex-row">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
       </main>
     </>
   )
