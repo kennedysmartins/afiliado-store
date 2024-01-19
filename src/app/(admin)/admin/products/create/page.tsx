9913cfd5b031b578a223823828e808e3332e5897
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-19 04:22:03 -0300

    form create product

--- src/app/(admin)/admin/products/create/page.tsx
+++ src/app/(admin)/admin/products/create/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { FormAddProduct } from "@/components/FormAddProduct"
+import React from "react"
 
 const CreateProduct = () => {
   return (
-    <main className="mx-10 max-w-5xl text-2xl gap-2 my-10">CreateProduct</main>
+    <main className="mx-10 max-w-5xl text-2xl gap-2 my-10">
+      Crie uma promo
+      <div className="flex w-[23rem]">
+        <FormAddProduct />
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/FormAddProduct.tsx
@@ -0,0 +1,185 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  urlProduct: z.string().min(2, {
+    message: "urlProduct must be at least 2 characters.",
+  }),
+  buyLink: z.string().min(2, {
+    message: "buyLink must be at least 2 characters.",
+  }),
+  nameProduct: z.string().min(2, {
+    message: "nameProduct must be at least 2 characters.",
+  }),
+  conditionPayment: z.string().min(2, {
+    message: "conditionPayment must be at least 2 characters.",
+  }),
+  currentPrice: z.string().min(2, {
+    message: "currentPrice must be at least 2 characters.",
+  }),
+  website: z.string().min(2, {
+    message: "website must be at least 2 characters.",
+  }),
+  oldPrice: z.string().min(2, {
+    message: "oldPrice must be at least 2 characters.",
+  }),
+})
+
+export function FormAddProduct() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      urlProduct: "",
+      buyLink: "",
+      nameProduct: "",
+      conditionPayment: "",
+      oldPrice: "",
+      currentPrice: "",
+      website:""
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 mt-10 w-full"
+      >
+        <FormField
+          control={form.control}
+          name="urlProduct"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>URL</FormLabel>
+              <FormControl>
+                <div className="flex w-full max-w-sm items-center space-x-2">
+                  <Input placeholder="https://..." {...field} />
+
+                  <Button type="submit">Analisar</Button>
+                </div>
+              </FormControl>
+              <FormDescription>Cole a URL que deseja analisar.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Separator />
+
+        <FormField
+          control={form.control}
+          name="nameProduct"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome do produto</FormLabel>
+              <FormControl>
+                <Input placeholder="iPhone 15 Pro 256GB Preto" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="conditionPayment"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Condições</FormLabel>
+              <FormControl>
+                <Input placeholder="no PIX ou em 10x sem júros" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex w-full min-w-full gap-2">
+          <FormField
+            control={form.control}
+            name="oldPrice"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preço Antigo</FormLabel>
+                <FormControl>
+                  <Input placeholder="12.220,99" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="currentPrice"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preço</FormLabel>
+                <FormControl>
+                  <Input placeholder="10.220,99" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="buyLink"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Link de compra</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="https://amazon.com.br/DSKJADSJ"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="website"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Website</FormLabel>
+              <FormControl>
+                <Input placeholder="Amazon" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Criar Produto</Button>
+      </form>
+    </Form>
+  )
+}
