985f5bf54f37b5fdd0c006abd009c6e509919770
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-20 17:17:37 -0300

    update layout lang

--- src/app/(content)/[id]/page.tsx
+++ src/app/(content)/[id]/page.tsx
@@ -1,18 +1,17 @@
 import { CarouselHome } from "@/components/CarouselHome"
 import Navbar from "@/components/Navbar"
 import ProductCard from "@/components/ProductCard"
-type Params = {
-  params: { id: string }
-  id: string
-}
+// type Params = {
+//   params: { id: string }
+//   id: string
+// }
 
-export default function ProductPage({ params: { id } }: Params) {
+export default function ProductPage({ params: { id } }: any) {
   return (
     <>
       <Navbar />
       <main className="mx-auto sm:w-[83rem] text-2xl gap-2 my-10 w-full">
         <div className="flex justify-center items-center w-full">
-          <CarouselHome />
         </div>
 
         <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-8 w-full mx-auto sm:flex-row">
