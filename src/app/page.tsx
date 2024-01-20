b02632eaab2b40e4762e69322343936e367f2185
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-20 17:06:03 -0300

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AnnouncementCard from "@/components/AnnouncementCard"
 import { CarouselHome } from "@/components/CarouselHome"
 import Navbar from "@/components/Navbar"
 import ProductCard from "@/components/ProductCard"
@@ -6,28 +7,59 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="mx-auto sm:w-[83rem] text-2xl gap-2 my-10 w-full">
-        <div className="flex justify-center items-center w-full">
-        <CarouselHome />
+      <main className="flex flex-col mx-auto text-2xl gap-2 my-10 w-[80%] items-center justify-center">
+        <div className="container flex justify-center items-center mx-4">
+          <CarouselHome />
         </div>
 
-        <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-8 w-full mx-auto sm:flex-row">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+        <div className="container flex flex-col sm:flex-row sm:w-full">
+          <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-3 sm:w-2/3 mx-auto sm:flex-row">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+          <div className="flex mx-auto">
+            <div className="flex flex-col w-full sm:w-1/3 gap-4">
+
+              <AnnouncementCard
+                data={{
+                  cardTitle: "Receba no WhatsApp",
+                  description:
+                    "Acompanhe as melhores promoções da internet no seu WhatsApp",
+                  footer: "Receba as melhores",
+                }}
+              />
+              <AnnouncementCard
+                data={{
+                  cardTitle: "Receba no Instagram",
+                  description:
+                    "Siga a gente no Instagram e não perca mais nenhuma oferta!",
+                  footer: "Seguir perfil",
+                }}
+              />
+              <AnnouncementCard
+                data={{
+                  cardTitle: "Receba no Telegram",
+                  description:
+                    "Nosso canal no telegram também manda todas as promoções",
+                  footer: "Entrar no canal",
+                }}
+              />
+            </div>
+          </div>
         </div>
       </main>
     </>
