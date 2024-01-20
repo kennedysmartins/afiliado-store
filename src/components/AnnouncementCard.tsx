6b4084fd2a3b02bd5f8f8c809501870f7073da64
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-20 17:06:34 -0300

    feat AnnoucementCard

---
+++ src/components/AnnouncementCard.tsx
@@ -0,0 +1,40 @@
+"use client"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "./ui/button"
+
+//create type data structure
+type cardType = {
+  data : {
+    cardTitle: string
+    description: string
+    footer: string
+  }
+}
+
+export default function AnnouncementCard({data}:cardType) {
+  const { cardTitle, description, footer } = data
+  return (
+    <div>
+      <Card className="w-72 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
+        <CardContent className="p-4">
+          <CardTitle className="text-xl font-semibold mb-2 text-center">
+            {cardTitle}
+          </CardTitle>
+          <CardDescription className="text-center">
+            {description}
+          </CardDescription>
+        </CardContent>
+        <CardFooter className="p-4 flex justify-center items-center">
+          <Button>{footer}</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
