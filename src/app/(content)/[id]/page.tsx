11ad5499f375b623847cd3ee176ae12e5ea9bf16
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-23 20:59:44 -0300

    product page

--- src/app/(content)/[id]/page.tsx
+++ src/app/(content)/[id]/page.tsx
@@ -1,21 +1,18 @@
 import { CarouselHome } from "@/components/CarouselHome"
 import Navbar from "@/components/Navbar"
 import ProductCard from "@/components/ProductCard"
-// type Params = {
-//   params: { id: string }
-//   id: string
-// }
+import ProductHero from "@/components/ProductHero"
 
-export default function ProductPage({ params: { id } }: any) {
+export default function ProductPage({ params }: { params: { id: string } }) {
+  const id:string = params.id
   return (
     <>
       <Navbar />
       <main className="mx-auto sm:w-[83rem] text-2xl gap-2 my-10 w-full">
-        <div className="flex justify-center items-center w-full">
-        </div>
+        <div className="flex justify-center items-center w-full"></div>
 
         <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-8 w-full mx-auto sm:flex-row">
-
+          <ProductHero id={id} />
         </div>
       </main>
     </>
