a337e458e35c1d08cb3f593966f2c2cfada48c6b
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-23 02:55:59 -0300

    productCard types and values

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,28 +9,31 @@ import {
 } from "@/components/ui/card"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { Product } from "@/lib/types"
 
-export default function ProductCard() {
+export default function ProductCard({ product }: { product: Product }) {
   return (
     <div>
       <Card className="w-52 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300">
         <CardHeader className="relative">
           <Link href="#">
             <img
-              src="https://http2.mlstatic.com/D_NQ_NP_881016-MLM51559383738_092022E-O.webp"
-              alt="Product Image"
-              className="w-full h-48 bg-contain bg-center object-contain bg-white rounded hover:scale-105 transition duration-300"
+              src={product.image}
+              alt={product.title}
+              className="w-full h-48 bg-contain bg-center object-cover bg-white rounded hover:scale-105 transition duration-300 p-2"
             />
           </Link>
         </CardHeader>
         <Link href="#">
           <CardContent className="p-4">
-            <CardTitle className="text-sm font-semibold mb-2">
-              Iphone 13 De 128 Gb Cor Meia Noite Apple - Distribuidor Autorizado
+            <CardTitle className="text-sm font-semibold mb-2 h-12 overflow-hidden">
+              {product.title}
             </CardTitle>
-            <CardDescription className="mb-4">Frete grátis</CardDescription>
-            <p className="text-sm line-through opacity-55">R$139,99</p>
-            <p className=" font-medium mb-2">R$99,99</p>
+            <CardDescription className="mb-4 h-4 overflow-hidden">
+              Frete grátis
+            </CardDescription>
+            <p className="text-sm line-through opacity-55 h-4 overflow-hidden">{`R$ ${product.originalPrice}`}</p>
+            <p className=" font-medium mb-2 h-8 overflow-hidden">{`R$ ${product.currentPrice}`}</p>
           </CardContent>
         </Link>
 
