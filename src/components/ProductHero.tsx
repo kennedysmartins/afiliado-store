a370e50a6f2613e0e1b1f173efb74c959a1cb685
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-26 17:43:15 -0300

    productPage

--- src/components/ProductHero.tsx
+++ src/components/ProductHero.tsx
@@ -2,8 +2,18 @@
 "use client"
 import * as React from "react"
 import { fetchProduct } from "@/lib/api"
-import ProductCard from "@/components/ProductCard"
 import { Product } from "@/lib/types"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card"
+import Link from "next/link"
+import { formatCurrency } from "@/lib/utils"
+import { Button } from "./ui/button"
+import ProductList from "./ProductList"
 
 const ProductHero = ({ id }: { id: string }) => {
   const [product, setProduct] = React.useState<Product | null>(null)
@@ -25,10 +35,51 @@ const ProductHero = ({ id }: { id: string }) => {
     fetchProductAPI()
   }, [fetchProductAPI])
   return (
-    <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-3 sm:w-2/3 mx-auto sm:flex-row">
-      {product && <ProductCard key={product.customId} product={product} />}
+    <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-3 sm:w-2/3 mx-auto">
+      {product && (
+        <div className="flex flex-col justify-center items-center gap-10">
+          <Card className="w-full overflow-hidden transition duration-300 border-none">
+            <div className="flex w-full border-none">
+              <CardHeader className="relative w-1/2 lg:w-full">
+                <Link href={`${product.buyLink}`}>
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="w-48 h-48 bg-contain bg-center object-contain bg-white rounded hover:scale-105 transition duration-300 p-2"
+                  />
+                </Link>
+              </CardHeader>
+              <CardContent className="p-4 w-1/2 lg:w-full ">
+                <Link href={`${product.buyLink}`}>
+                  <CardTitle className="text-sm font-semibold mb-2 overflow-hidden">
+                    {product.title}
+                  </CardTitle>
+                  <CardDescription className="mb-4 my-4">
+                    Frete grátis
+                  </CardDescription>
+                  <p className="text-sm line-through opacity-55">{`R$ ${formatCurrency(
+                    product.originalPrice
+                  )}`}</p>
+                  <p className="md:font-medium mb-2 h-8 text-primary ">{`R$ ${formatCurrency(
+                    product.currentPrice
+                  )}`}</p>
+                </Link>
+
+                <Link href={`${product.buyLink}`}>
+                  <Button className="mt-2">Pegar promoção</Button>
+                </Link>
+              </CardContent>
+            </div>
+          </Card>
+          <div className="flex flex-col align-middle justify-center gap-3 container">
+            <h2 className="text-center">Últimas Promoções</h2>
+            <ProductList limit={3} />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
+
 
 export default ProductHero
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -6,20 +6,18 @@ import ProductCard from "@/components/ProductCard"
 import { Product } from "@/lib/types"
 
 // Componente funcional ProductList
-const ProductList = () => {
-
-
+const ProductList = ({ limit = 0 }) => {
   // Estado para armazenar os produtos da pesquisa
   const [products, setProducts] = React.useState<Product[]>([])
 
   // Função para realizar a pesquisa de produtos usando a API
   const fetchProductsAPI = React.useCallback(async () => {
     try {
-        const response: Product[] = await fetchProducts()
-        if (response) {
-          console.log(response)
-          setProducts(response)
-        }
+      const response: Product[] = await fetchProducts()
+      if (response) {
+        console.log(response)
+        setProducts(response)
+      }
     } catch (error) {
       console.error("Erro ao buscar produtos", error)
       setProducts([])
@@ -34,11 +32,11 @@ const ProductList = () => {
 
   // Renderiza o componente
   return (
-      <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-3 sm:w-2/3 mx-auto sm:flex-row">
-        {products.map((product) => (
-          <ProductCard key={product.customId} product={product} />
-        ))}
-      </div>
+    <div className="flex flex-col flex-wrap justify-center items-center align-middle gap-3 sm:w-2/3 mx-auto sm:flex-row">
+      {products.slice(0, limit > 0 ? limit : products.length).map((product) => (
+        <ProductCard key={product.customId} product={product} />
+      ))}
+    </div>
   )
 }
 
