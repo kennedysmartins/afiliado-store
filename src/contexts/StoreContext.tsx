5fd7b16645a5b05866b7b14b2c4a424a71757d22
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-26 03:57:28 -0300

    define theme on store load

--- src/contexts/StoreContext.tsx
+++ src/contexts/StoreContext.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { createDefaultStoreConfigs, getAllStoreConfigs } from "@/lib/api"
 import { Progress } from "@/components/ui/progress"
+import { StoreInfo } from "@/lib/types"
 
 import {
   createContext,
@@ -10,15 +11,7 @@ import {
   useEffect,
   useCallback,
 } from "react"
-
-type StoreInfo = {
-  id: string
-  storeName: string
-  storeDescription: string
-  storeLogo: string | null
-  storeContact: string | null
-  storeConfig: any | null
-}
+import { useTheme } from "next-themes"
 
 type StoreContextType = {
   storeInfo: StoreInfo | null
@@ -29,12 +22,12 @@ export const StoreContext = createContext<StoreContextType | null>(null)
 export const StoreProvider = ({ children }: { children: ReactNode }) => {
   const [storeInfo, setStoreInfo] = useState<StoreInfo | null>(null)
   const [isLoading, setIsLoading] = useState(0)
+  const { theme, setTheme } = useTheme()
 
   const loadStore = useCallback(async () => {
     try {
       setIsLoading(33)
       setIsLoading(80)
-
 
       const infoList = await getAllStoreConfigs()
       setIsLoading(90)
@@ -51,6 +44,10 @@ export const StoreProvider = ({ children }: { children: ReactNode }) => {
         }
       } else {
         setStoreInfo(infoList[0])
+        if(theme == "none") {
+          setTheme(infoList[0].storeConfig.color)
+        }
+
         setIsLoading(90)
       }
     } finally {
@@ -66,9 +63,8 @@ export const StoreProvider = ({ children }: { children: ReactNode }) => {
     return (
       <>
         <div className="flex justify-center items-center h-dvh">
-          <div className="w-1/4">
-
-          <Progress value={isLoading} />
+          <div className="w-32">
+            <Progress value={isLoading} />
           </div>
         </div>
       </>
