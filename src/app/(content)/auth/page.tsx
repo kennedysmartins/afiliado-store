18aaf1ba1ac3cef6ff3daddd5adccdd22e9bcd93
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-28 00:57:17 -0300

    login image

--- src/app/(content)/auth/page.tsx
+++ src/app/(content)/auth/page.tsx
@@ -14,7 +14,7 @@ export default function AuthenticationPage() {
     <>
       <div className="container relative h-dvh flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="absolute inset-0 bg-[url('/login.jpg')] bg-center" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +30,7 @@ export default function AuthenticationPage() {
             </svg>
             {/* Afiliado Store */}
           </div>
-          <div className="relative z-20 mt-auto">
+          <div className="relative z-20 mt-auto ">
             <blockquote className="space-y-2">
               <p className="text-lg">
                 &ldquo;Nunca foi tão fácil criar uma loja de produtos
