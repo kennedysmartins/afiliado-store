02d3bb00d7aab0d571a7efaa984cd4abf983fc9d
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-28 14:45:16 -0300

    fix padding

--- src/app/(content)/auth/page.tsx
+++ src/app/(content)/auth/page.tsx
@@ -36,7 +36,7 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <Tabs defaultValue="Criar conta" className="space-y-4">
               <TabsList>
