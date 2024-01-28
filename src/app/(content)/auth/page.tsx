5749654c2decb25ce261086c39fc8fa7928bbbaf
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-28 14:36:28 -0300

    fix height

--- src/app/(content)/auth/page.tsx
+++ src/app/(content)/auth/page.tsx
@@ -9,7 +9,7 @@ export default function AuthenticationPage() {
     <>
     <Navbar />
       <div className="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+        <div className="relative hidden h-full min-h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-[url('/login.png')] bg-center" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
