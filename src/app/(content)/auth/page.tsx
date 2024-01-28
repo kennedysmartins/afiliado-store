4971bd1fdf1a195fd113ba66719e6f773a95aeb2
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-28 14:34:43 -0300

    fix style auth page

--- src/app/(content)/auth/page.tsx
+++ src/app/(content)/auth/page.tsx
@@ -8,7 +8,7 @@ export default function AuthenticationPage() {
   return (
     <>
     <Navbar />
-      <div className="container relative h-dvh flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-[url('/login.png')] bg-center" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -36,7 +36,7 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <Tabs defaultValue="Criar conta" className="space-y-4">
               <TabsList>
